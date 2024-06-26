e6afa813739d43cb154528d3f6f25e98efb0411c
Nathaniel <nathjoeetim@gmail.com>
2024-06-26 17:28:01 +0100

    first commit

--- src/assets/data/blogData.js
+++ src/assets/data/blogData.js
@@ -6,41 +6,41 @@ import img03 from "../all-images/blog-img/blog-3.jpg";
 const blogData = [
   {
     id: 1,
-    title: "The best way to drive cars",
-    author: "Muhib",
-    date: "12 Dec, 2020",
+    title: "The best way to drive an ambulance",
+    author: "jonas J.",
+    date: "12 Dec, 2023",
     time: "9pm",
     imgUrl: img01,
     description:
-      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+      " Driving an ambulance requires a unique set of skills to ensure patient safety and timely arrival at the destination. The best way to drive an ambulance includes completing the necessary training programs and obtaining certification in emergency vehicle operations. It is crucial to maintain composure during high-stress situations to make clear and quick decisions. Following all traffic laws and regulations, while using lights and sirens appropriately to navigate through traffic safely, is essential. Anticipating the actions of other drivers and always having an escape route planned helps avoid potential hazards. Ensuring smooth acceleration, braking, and cornering provides a stable environment for the patient and medical personnel in the back. Additionally, staying in constant communication with dispatch and other emergency vehicles is vital to coordinate efforts effectively. By following these guidelines, ambulance drivers can ensure the safety of their patients and themselves while providing efficient emergency medical services.",
     quote:
-      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+      "Driving an ambulance is not just about speed; it's about precision, calm under pressure, and the unwavering commitment to saving lives.",
   },
 
   {
     id: 2,
-    title: "If your car battery is down",
-    author: "Muhib",
-    date: "12 Dec, 2020",
+    title: "How to create way for an ambulance",
+    author: "David O.",
+    date: "12 AUG, 2023",
     time: "9pm",
     imgUrl: img02,
     description:
-      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+      "Creating a way for an ambulance is crucial to ensure that emergency medical services reach their destination quickly and safely. When you hear an ambulance siren or see its flashing lights, stay calm and alert. Safely check your surroundings and look for the ambulance. If you are driving, slow down and pull over to the right side of the road. If you cannot pull over immediately, continue driving carefully until you can do so safely. Do not stop in the middle of the road or at an intersection, as this can obstruct the ambulance's path. Avoid making sudden movements that could confuse other drivers. If you are at a red light, remain where you are until the ambulance passes, unless directed otherwise by emergency personnel. For pedestrians, stay on the sidewalk and avoid crossing the street until the ambulance has passed. Always follow traffic laws and signals, and never try to outrun or follow an ambulance. Your cooperation helps ensure that emergency responders can reach those in need as quickly as possible, potentially saving lives.",
     quote:
-      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+      "When seconds count, making way for an ambulance isn't just about following the rules; it's about giving the gift of time to those in need.",
   },
 
   {
     id: 3,
-    title: "The best way to give trip",
-    author: "Muhib",
-    date: "12 Dec, 2020",
+    title: "When to go for medical consultation",
+    author: "Joy O. O.",
+    date: "12 May, 2024",
     time: "9pm",
     imgUrl: img03,
     description:
-      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.  Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+      "Knowing when to seek medical consultation is crucial for maintaining your health. If you experience persistent or severe symptoms, changes in your health that concern you, or if you have questions about your well-being, it's advisable to seek medical advice. Timely consultation can help identify underlying issues early, allowing for prompt treatment and improved health outcomes. Taking proactive steps towards your health ensures you receive the care and guidance needed to stay well-informed and healthy.",
     quote:
-      "Voluptua dolore takimata dolor sadipscing nonumy consetetur et. Stet sed dolores dolores dolores dolores eos.",
+      "Seeking medical consultation is not just about addressing symptoms; it's about taking proactive steps towards a healthier, informed life.",
   },
 ];
 
--- src/components/UI/Testimonial.jsx
+++ src/components/UI/Testimonial.jsx
@@ -41,65 +41,95 @@ const Testimonial = () => {
   return (
     <Slider {...settings}>
       <div className="testimonial py-4 px-3">
-        <p className="section__description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magni
-          explicabo molestias recusandae repudiandae, dolor, sapiente placeat
-          ab, animi eum minima nulla facere aliquam aut vitae quo pariatur
-          voluptate odit?
+        <p
+          className="section__description"
+          style={{
+            textAlign: "center",
+            height: "200px",
+            overflowY: "hidden",
+          }}
+        >
+          I am extremely thankful for the quick medication delivery service.
+          When I needed my prescription urgently, the delivery was prompt and
+          hassle-free. The process was smooth, from placing the order online to
+          receiving my medications at my doorstep. Thank you for your
+          exceptional service and reliability!"
         </p>
 
         <div className="mt-3 d-flex align-items-center gap-4">
           <img src={ava01} alt="" className="w-25 h-25 rounded-2" />
 
           <div>
-            <h6 className="mb-0 mt-3">Jhon Doe</h6>
+            <h6 className="mb-0 mt-3">Jane D</h6>
             <p className="section__description">Customer</p>
           </div>
         </div>
       </div>
 
       <div className="testimonial py-4 px-3">
-        <p className="section__description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magni
-          explicabo molestias recusandae repudiandae, dolor, sapiente placeat
-          ab, animi eum minima nulla facere aliquam aut vitae quo pariatur
-          voluptate odit?
+        <p
+          className="section__description"
+          style={{
+            textAlign: "center",
+            height: "200px",
+            overflowY: "hidden",
+          }}
+        >
+          I am incredibly grateful for the fast and efficient ambulance service
+          . When I called for help during a medical emergency, the response was
+          immediate.Quickly and were extremely professional and compassionate.
+          Thank you for being there when we needed you the most!
         </p>
 
         <div className="mt-3 d-flex align-items-center gap-4">
           <img src={ava02} alt="" className="w-25 h-25 rounded-2" />
 
           <div>
-            <h6 className="mb-0 mt-3">Jhon Doe</h6>
+            <h6 className="mb-0 mt-3">John S.</h6>
             <p className="section__description">Customer</p>
           </div>
         </div>
       </div>
 
       <div className="testimonial py-4 px-3">
-        <p className="section__description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magni
-          explicabo molestias recusandae repudiandae, dolor, sapiente placeat
-          ab, animi eum minima nulla facere aliquam aut vitae quo pariatur
-          voluptate odit?
+        <p
+          className="section__description"
+          style={{
+            textAlign: "center",
+            height: "200px",
+            overflowY: "hidden",
+          }}
+        >
+          I want to express my gratitude for the exceptional medical consultancy
+          service. The online consultation was convenient and thorough They took
+          the time to understand my concerns and provided clear, personalized
+          advice that greatly helped me manage my condition.
         </p>
 
         <div className="mt-3 d-flex align-items-center gap-4">
           <img src={ava03} alt="" className="w-25 h-25 rounded-2" />
 
           <div>
-            <h6 className="mb-0 mt-3">Jhon Doe</h6>
+            <h6 className="mb-0 mt-3">Mary A.</h6>
             <p className="section__description">Customer</p>
           </div>
         </div>
       </div>
 
       <div className="testimonial py-4 px-3">
-        <p className="section__description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus magni
-          explicabo molestias recusandae repudiandae, dolor, sapiente placeat
-          ab, animi eum minima nulla facere aliquam aut vitae quo pariatur
-          voluptate odit?
+        <p
+          className="section__description"
+          style={{
+            textAlign: "center",
+            height: "200px",
+            overflowY: "hidden",
+          }}
+        >
+          I am incredibly thankful for the medical consultancy service.
+          Scheduling an appointment was easy, and I was able to speak with a
+          doctor from the comfort of my home. The consultation was thorough, and
+          the doctor was attentive and understanding. Thank you for your
+          excellent care and support!
         </p>
 
         <div className="mt-3 d-flex align-items-center gap-4">
